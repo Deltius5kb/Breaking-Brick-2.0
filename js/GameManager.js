9658925cde8197cbc9006e3442a30a9140bc97fa
Armin Raad <45851390+Deltius5kb@users.noreply.github.com>
2023-03-03 00:00:39 +0000

    title screen layout done

--- js/GameManager.js
+++ js/GameManager.js
@@ -7,20 +7,17 @@ class GameManager
 
     constructor() 
     {
-        this.#m_GameObject = new Game();
     }
 
     // Called every frame from main.js animate()
     update(f_Time) 
     {
         this.#get_delta_time(f_Time);
-        this.#m_GameObject.update(this.#f_DeltaTime);
     }
 
     // Called every frame from main.js animate()
     draw()
     {
-        this.#m_GameObject.draw();
     }
 
     // Called from update() every frame 
